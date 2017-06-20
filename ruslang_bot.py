aac8615bd386ac04be398503586dc9706ec56bdf
Natasha Ermolaeva <litegoldfish@yandex.ru>
2017-06-20 23:46:51 +0300

    Update ruslang_bot.py

--- ruslang_bot.py
+++ ruslang_bot.py
@@ -23,24 +23,32 @@ bot.set_webhook(url="https://sheltered-plateau-79499.herokuapp.com/bot")
 app = flask.Flask(__name__)
 
 lang_iso = ['abq', 'agx', 'ady', 'ava','ale', 'alr', 'ams', 'ani', 'aqc', 'akv', 'kva', 'bak', 'kap', 'bph', 'bxr', 'vep', 'vvk', 'vot', 'gap', 'gin', 'gdo', 'mrj', 'huz', 'izh', 'inh', 'itl', 'kbd', 'kad', 'kaj', 'xal', 'kpt', 'krc', 'krl', 'ket', 'sjd', 'kpv', 'koi', 'kpy', 'kas', 'kum', 'lbe', 'lez', 'for', 'dar', 'mhr', 'mns', 'meg', 'mdf', 'mui', 'gld', 'nio', 'neg', 'niv', 'nog', 'oaa', 'rut', 'its', 'atv', 'ykg', 'sel', 'stc', 'tab', 'tsr', 'tat', 'ttt', 'tin', 'kim', 'tub', 'tyv', 'yrk', 'udi', 'udm', 'ude', 'ulc', 'utc', 'kjh', 'kca', 'khv', 'tkr', 'ddo', 'rmy', 'cji', 'che', 'chi', 'chv', 'ckt', 'clw', 'cjs', 'evn', 'eve', 'enf', 'myv', 'ess', 'alt', 'yux', 'sah']
-# lang_iso = ['alt']
 
 def lang_page():
     values = {}
-    common_url = 'http://web-corpora.net/wsgi3/minorlangs/view/'
-    for iso in lang_iso:
-        lang_url = common_url + iso
-        user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'
-        req = urllib.request.Request(lang_url, headers={'User-Agent':user_agent})
-        try:
-            with urllib.request.urlopen(req) as response:
-                lang_resp = response.read().decode('utf-8')
-                # f = open('langs' + os.sep + str(iso) + '.html', 'w')
-                # f.write(lang_resp)
-                # f.close()
-        except HTTPError as e:
-            content = e.read()
 
+    # создаем локальные копии всех доступных страниц с языками (чтобы не нагружать сайт при каждом сообщении пользователя боту)
+
+    # common_url = 'http://web-corpora.net/wsgi3/minorlangs/view/'
+    for iso in lang_iso:
+        # lang_url = common_url + iso
+        # user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'
+        # req = urllib.request.Request(lang_url, headers={'User-Agent':user_agent})
+        # try:
+        #     with urllib.request.urlopen(req) as response:
+        #         lang_resp = response.read().decode('utf-8')
+        #         # f = open('langs' + os.sep + str(iso) + '.html', 'w')
+        #         # f.write(lang_resp)
+        #         # f.close()
+        # except HTTPError as e:
+        #     content = e.read()
+        try:
+            with open('langs' + os.sep + iso + '.html', 'r') as f:
+                lang_resp = f.read()
+        except:
+            print('no lang file ' + iso)
+
+        # таким способом информация достается быстрее, нежели регулярными выражениями
         root = html.fromstring(lang_resp)
         nodes = root.xpath(".//table[contains(@class, 'lang-info')]//tr")
         values[iso] = {}
@@ -87,41 +95,62 @@ def make_dict(values, lang_iso):
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, 'Здравствуйте! Это бот, который может рассказать некоторую информацию о любом из языков России. Пожалуйста, введите название или самоназвание (они должны быть напечатаны в нижнем регистре) интересующего вас языка. В случае затруднения воспользуйтесь командой /help.')
+    bot.send_message(message.chat.id, 'Здравствуйте! Это бот, который может рассказать некоторую информацию о любом из языков России.\n\n\
+Пожалуйста, введите название или самоназвание (или какую-либо их часть; главное, чтобы все было напечатано в нижнем регистре) \
+интересующего вас языка (без слова "язык"). \n\nВ случае затруднения воспользуйтесь командой /help.')
 
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, 'Чтобы начать использование бота, вам нужно лишь отправить ему команду /start, а затем \u2013 название (или самоназвание) интересующего вас языка, а бот, в свою очередь, расскажет все, что знает об этом языке.')
+    bot.send_message(message.chat.id, 'Чтобы начать использование бота, вам нужно лишь отправить ему команду /start, \
+а затем \u2013 название/самоназвание (или их часть, например, для южноалтайского языка можно написать лишь "алт") интересующего вас языка, \
+а бот, в свою очередь, расскажет все, что знает об этом языке, \
+а также распечатает список языков, названия или самоназвания которых хотя бы частично совпадают с вашим запросом.')
 
 @bot.message_handler(func=lambda m: True)
 def send_info(message):
-    bot.send_message(message.chat.id, 'Пожалуйста, немного подождите, я ищу информацию по вашему запросу.')
-    bot.send_sticker(message.chat.id, 'CAADAgADiQADhC64BjZ1mCglHfVbAg')
+    chat_id = message.chat.id
+    bot.send_chat_action(chat_id, 'typing')
+    bot.send_sticker(chat_id, 'CAADAgADiQADhC64BjZ1mCglHfVbAg')
     v = lang_page()
     r = make_dict(v, lang_iso)
 
-    # [message.text for key, value in r.items() if message.text in key]
+    a = [value for key, value in r.items() if key.find(message.text) != -1]
+    try:
+        lang = a[0]
+    except:
+        try:
+            lang = r[message.text]
+        except:
+            lang = ""
 
-    if message.text in r:
-        message_value = r[message.text]
-        print(message_value)
-        bot.send_message(message.chat.id, 'Вот, что мне удалось найти про ' + message.text + ':\n')
+    if lang is not "":
+        result = ''
         for k in v:
-            if message_value in v[k]['Язык:']:
+            if lang in v[k]['Язык:']:
+                if v[k]['Самоназвание:'] is not "":
+                    selfn = ' (' + v[k]['Самоназвание:'] + ')'
+                else:
+                    selfn = ''
+
+                result += 'Вот, что мне удалось найти про ' + lang + selfn + ':\n\n'
+
                 c = dict(v[k])
                 del c['Язык:']
                 del c['Самоназвание:']
                 for x, y in c.items():
-                    time.sleep(0.1)
-                    bot.send_message(message.chat.id, x + ' ' + y)
+                    if y is not "":
+                        result += x + ' ' + y + '\n'
+                break
 
+        bot.send_message(chat_id, result)
+
+        others = set(a)
+        if len(others) > 1:
+            others.remove(lang)
+            bot.send_message(chat_id, 'Другие языки по вашему запросу: ' + ", ".join(others))
     else:
-        bot.send_message(message.chat.id, 'К сожалению, в моей базе данных отсутствует информация об этом языке :(\nМожет быть, вы попробуете ввести другой запрос или проверите свой на наличие заглавных букв и опечаток? :)')
+        bot.send_message(chat_id, 'К сожалению, в моей базе данных отсутствует информация об этом языке :(\nМожет быть, вы попробуете ввести другой запрос или проверите свой на наличие заглавных букв и опечаток?')
 
-
-@app.route("/", methods=['GET', 'HEAD'])
-def index():
-    return 'ok'
 
 @app.route("/bot", methods=['POST'])
 def webhook():
